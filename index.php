f4180a6020fd8d549f1f02535c5c1fb7f016459f
radek24 <76497057+radek24@users.noreply.github.com>
2022-03-12 00:59:57 +0100

    pokusy s Bootstrapem

--- index.php
+++ index.php
@@ -1,26 +1,67 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- The page supports both dark and light color schemes,
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- The page supports both dark and light color schemes,
          and the page author prefers / default is light. -->
-    <meta name="color-scheme" content="light dark">
+  <meta name="color-scheme" content="dark">
 
-    <!-- Replace the Bootstrap CSS with the
-         Bootstrap-Dark Variant CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-nightshade.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-nightshade.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Hello, world!</title>
+</head>
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello word</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt optio consectetur dolore, fuga tenetur non aliquam, hic quod nobis quasi provident sequi voluptatum commodi. Inventore necessitatibus quam quas distinctio ullam.</p>
-    <!--foooter tady-->
-    <script src="location/of/the/bootstrap.js/here"></script>
+<body>
+  <div class="container pt-5 ">
+    <div class="text-center">
+      <h1 class="display-1" style="font-weight: bold;">BAND</h1>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/js/darkmode.min.js"></script>
-  </body>
+      <div class="container">
+        <div class="row text-muted">
+
+          <div class="col-sm-6 ">
+            <h4>X ALBUMS</h4>
+          </div>
+          <div class="col-sm-6">
+            <h4>X SONGS</h4>
+          </div>
+        </div>
+      </div>
+
+      <h3 class="pb-5">info about band</h3>
+    </div>
+    <div class="card"  >
+      <div class="card-body">
+        <div class="container">
+          <div class="row">
+            
+          <div class="col-sm-2" style="padding-left:0">
+              <img class="img-fluid rounded" src="Img/LoremImg.jpg">
+            </div>
+            <div class="col-sm-6">
+              <h3>ALBUM NAME</h3>
+              <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
+              <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
+            </div>
+            <div class="col">
+              <h4 class="pt-5">Average rating</h4>
+              <p class="pt-0" style="font-size:50px ;color:yellow">******</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!--foooter tady-->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/js/darkmode.min.js"></script>
+</body>
+
 </html>
