c943e26ee64f177ce58a134bdfe8d63872339723
radek24 <76497057+radek24@users.noreply.github.com>
2022-03-11 15:42:21 +0100

    bootstrap

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
-<!DOCTYPE html>
-<html lang="cs">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ZWA</title>
-</head>
-<body>
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- The page supports both dark and light color schemes,
+         and the page author prefers / default is light. -->
+    <meta name="color-scheme" content="light dark">
+
+    <!-- Replace the Bootstrap CSS with the
+         Bootstrap-Dark Variant CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-nightshade.min.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello word</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt optio consectetur dolore, fuga tenetur non aliquam, hic quod nobis quasi provident sequi voluptatum commodi. Inventore necessitatibus quam quas distinctio ullam.</p>
+    <!--foooter tady-->
+    <script src="location/of/the/bootstrap.js/here"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/js/darkmode.min.js"></script>
+  </body>
 </html>
