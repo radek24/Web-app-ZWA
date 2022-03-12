60430da13a74a6ad85289f579b226fcebbd75c90
radek24 <76497057+radek24@users.noreply.github.com>
2022-03-12 01:16:03 +0100

    neco

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  
+
   <!-- The page supports both dark and light color schemes,
          and the page author prefers / default is light. -->
   <meta name="color-scheme" content="dark">
@@ -16,30 +16,49 @@
 </head>
 
 <body>
-  <div class="container pt-5 ">
-    <div class="text-center">
-      <h1 class="display-1" style="font-weight: bold;">BAND</h1>
 
-      <div class="container">
-        <div class="row text-muted">
+  <nav class="navbar">
+    <div class="container-fluid">
+      <a class="navbar-brand">Navbar</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
 
-          <div class="col-sm-6 ">
-            <h4>X ALBUMS</h4>
-          </div>
-          <div class="col-sm-6">
-            <h4>X SONGS</h4>
+  <div class="band-container">
+    <div class="container pt-5 ">
+      <div class="text-center">
+        <h1 class="display-1 pt-5" style="font-weight: bold;">BAND</h1>
+
+        <div class="container">
+          <div class="row text-muted">
+
+            <div class="col-sm-6 ">
+              <h4>X ALBUMS</h4>
+            </div>
+            <div class="col-sm-6">
+              <h4>X SONGS</h4>
+            </div>
           </div>
         </div>
+        <h3 class="pb-5">info about band</h3>
       </div>
-
-      <h3 class="pb-5">info about band</h3>
     </div>
-    <div class="card"  >
+
+  </div>
+  <div class="container pt-5 ">
+    <div class="text-center">
+
+
+    </div>
+    <div class="card my-4">
       <div class="card-body">
         <div class="container">
           <div class="row">
-            
-          <div class="col-sm-2" style="padding-left:0">
+
+            <div class="col-sm-2" style="padding-left:0">
               <img class="img-fluid rounded" src="Img/LoremImg.jpg">
             </div>
             <div class="col-sm-6">
@@ -55,6 +74,96 @@
         </div>
       </div>
     </div>
+
+    <div class="card my-4">
+      <div class="card-body">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-sm-2" style="padding-left:0">
+              <img class="img-fluid rounded" src="Img/LoremImg.jpg">
+            </div>
+            <div class="col-sm-6">
+              <h3>ALBUM NAME</h3>
+              <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
+              <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
+            </div>
+            <div class="col">
+              <h4 class="pt-5">Average rating</h4>
+              <p class="pt-0" style="font-size:50px ;color:yellow">******</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card my-4">
+      <div class="card-body">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-sm-2" style="padding-left:0">
+              <img class="img-fluid rounded" src="Img/LoremImg.jpg">
+            </div>
+            <div class="col-sm-6">
+              <h3>ALBUM NAME</h3>
+              <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
+              <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
+            </div>
+            <div class="col">
+              <h4 class="pt-5">Average rating</h4>
+              <p class="pt-0" style="font-size:50px ;color:yellow">******</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card my-4">
+      <div class="card-body">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-sm-2" style="padding-left:0">
+              <img class="img-fluid rounded" src="Img/LoremImg.jpg">
+            </div>
+            <div class="col-sm-6">
+              <h3>ALBUM NAME</h3>
+              <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
+              <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
+            </div>
+            <div class="col">
+              <h4 class="pt-5">Average rating</h4>
+              <p class="pt-0" style="font-size:50px ;color:yellow">******</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card my-4">
+      <div class="card-body">
+        <div class="container">
+          <div class="row">
+
+            <div class="col-sm-2" style="padding-left:0">
+              <img class="img-fluid rounded" src="Img/LoremImg.jpg">
+            </div>
+            <div class="col-sm-6">
+              <h3>ALBUM NAME</h3>
+              <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
+              <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
+            </div>
+            <div class="col">
+              <h4 class="pt-5">Average rating</h4>
+              <p class="pt-0" style="font-size:50px ;color:yellow">******</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
   </div>
 
 
