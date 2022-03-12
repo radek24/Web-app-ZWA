4ab5bff42f2b59297bd9784c55fcb33d93950b45
radek24 <76497057+radek24@users.noreply.github.com>
2022-03-12 16:24:35 +0100

    neco

--- index.php
+++ index.php
@@ -1,176 +1,147 @@
 <!doctype html>
-<html lang="en">
+<html lang="cs">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- The page supports both dark and light color schemes,
-         and the page author prefers / default is light. -->
-  <meta name="color-scheme" content="dark">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-nightshade.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Hello, world!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-night.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>WebApp</title>
 </head>
 
 <body>
+<?php
+  $card = '      <div class="card my-4" >
+        <div class="card-body">
+            <div class="container">
+                <div class="row">
 
-  <nav class="navbar">
-    <div class="container-fluid">
-      <a class="navbar-brand">Navbar</a>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+                    <div class="col-md-2" style="padding-left:0">
+                        <img class="img-fluid rounded" src="Img/LoremImg.jpg">
+                    </div>
+                    <div class="col-md-6">
+                        <h3>ALBUM NAME</h3>
+                        <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
+                        <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
+                    </div>
+                    <div class="col-md-4">
+                        <h4 class="pt-5">Average rating</h4>
+                        <p class="pt-0" style="font-size:50px ;color:yellow">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  class="bi bi-star" viewBox="0 0 16 16">
+<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  class="bi bi-star" viewBox="0 0 16 16">
+<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  class="bi bi-star" viewBox="0 0 16 16">
+<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  class="bi bi-star" viewBox="0 0 16 16">
+<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  class="bi bi-star" viewBox="0 0 16 16">
+<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  class="bi bi-star" viewBox="0 0 16 16">
+<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>
+                      
+                      </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </nav>
+    '
 
-  <div class="band-container">
+
+?>
+<nav class="navbar navbar-expand-lg" style="position:absolute; background-color:var(--bs-body-bg);width:100%; z-index: 20;">
+  <a class="navbar-brand" href="#">RATER</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+    <div class="band-container">
+        <div class="container pt-5 ">
+            <div class="text-center">
+                <h1 class="display-1" style="font-weight: bold; padding-top:10%">BAND</h1>
+
+                <div class="container" >
+                    <div class="row text-muted">
+
+                        <div class="col-sm-6 ">
+                            <h4>X ALBUMS</h4>
+                        </div>
+                        <div class="col-sm-6">
+                            <h4>X SONGS</h4>
+                        </div>
+                    </div>
+                </div>
+                <h4 class="py-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolores voluptates sint sed expedita cupiditate, quia porro maiores laborum deleniti nesciunt! Neque sapiente non maxime impedit beatae nisi error aut.
+                </h4>
+            </div>
+        </div>
+
+    </div>
     <div class="container pt-5 ">
-      <div class="text-center">
-        <h1 class="display-1 pt-5" style="font-weight: bold;">BAND</h1>
+        <div class="text-center">
 
-        <div class="container">
-          <div class="row text-muted">
 
-            <div class="col-sm-6 ">
-              <h4>X ALBUMS</h4>
-            </div>
-            <div class="col-sm-6">
-              <h4>X SONGS</h4>
-            </div>
-          </div>
         </div>
-        <h3 class="pb-5">info about band</h3>
-      </div>
-    </div>
-
-  </div>
-  <div class="container pt-5 ">
-    <div class="text-center">
-
+        <?php
+        for ($i = 1; $i <= 4; $i++) {
+    echo $card;
+}
+?>
 
     </div>
-    <div class="card my-4">
-      <div class="card-body">
-        <div class="container">
-          <div class="row">
-
-            <div class="col-sm-2" style="padding-left:0">
-              <img class="img-fluid rounded" src="Img/LoremImg.jpg">
-            </div>
-            <div class="col-sm-6">
-              <h3>ALBUM NAME</h3>
-              <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
-              <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
-            </div>
-            <div class="col">
-              <h4 class="pt-5">Average rating</h4>
-              <p class="pt-0" style="font-size:50px ;color:yellow">******</p>
-            </div>
-          </div>
+    </div>
+    </div>
+    <footer id="sticky-footer" class="flex-shrink-0 py-3 bg-dark text-white-50 mt-5">
+        <div class="container text-center">
+            <small>Copyright 2022&copy; RATER</small>
         </div>
-      </div>
-    </div>
+    </footer>
 
-    <div class="card my-4">
-      <div class="card-body">
-        <div class="container">
-          <div class="row">
-
-            <div class="col-sm-2" style="padding-left:0">
-              <img class="img-fluid rounded" src="Img/LoremImg.jpg">
-            </div>
-            <div class="col-sm-6">
-              <h3>ALBUM NAME</h3>
-              <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
-              <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
-            </div>
-            <div class="col">
-              <h4 class="pt-5">Average rating</h4>
-              <p class="pt-0" style="font-size:50px ;color:yellow">******</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="card my-4">
-      <div class="card-body">
-        <div class="container">
-          <div class="row">
-
-            <div class="col-sm-2" style="padding-left:0">
-              <img class="img-fluid rounded" src="Img/LoremImg.jpg">
-            </div>
-            <div class="col-sm-6">
-              <h3>ALBUM NAME</h3>
-              <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
-              <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
-            </div>
-            <div class="col">
-              <h4 class="pt-5">Average rating</h4>
-              <p class="pt-0" style="font-size:50px ;color:yellow">******</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="card my-4">
-      <div class="card-body">
-        <div class="container">
-          <div class="row">
-
-            <div class="col-sm-2" style="padding-left:0">
-              <img class="img-fluid rounded" src="Img/LoremImg.jpg">
-            </div>
-            <div class="col-sm-6">
-              <h3>ALBUM NAME</h3>
-              <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
-              <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
-            </div>
-            <div class="col">
-              <h4 class="pt-5">Average rating</h4>
-              <p class="pt-0" style="font-size:50px ;color:yellow">******</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="card my-4">
-      <div class="card-body">
-        <div class="container">
-          <div class="row">
-
-            <div class="col-sm-2" style="padding-left:0">
-              <img class="img-fluid rounded" src="Img/LoremImg.jpg">
-            </div>
-            <div class="col-sm-6">
-              <h3>ALBUM NAME</h3>
-              <h6 class="card-subtitle mb-2 pt-1"><span class="badge bg-dark">Album</span></h6>
-              <h5 class="card-subtitle mb-2 text-muted pt-5">Song1, Song2, Sonng3 ...</h6>
-            </div>
-            <div class="col">
-              <h4 class="pt-5">Average rating</h4>
-              <p class="pt-0" style="font-size:50px ;color:yellow">******</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
-  </div>
-
-
-  <!--foooter tady-->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/js/darkmode.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/js/darkmode.min.js"></script>
 </body>
 
 </html>
