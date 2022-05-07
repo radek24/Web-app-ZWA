b0439a7c1caafe6a9c7abfe0e4af72d83053d107
Radek <76497057+radek24@users.noreply.github.com>
2022-05-07 20:49:07 +0200

    UX

--- login.php
+++ login.php
@@ -4,12 +4,21 @@ require("./phpFunc/DB_connect.php");
 require("./phpFunc/user.php");
 
 
-
+$chyba ="";
 if (isset($_POST["password"])) {
     $chyba = login($db, $_POST);
 }
 
 $titulek = "Rater - Login";
 require("./view/head.phtml");
-require("./view/login.phtml");
+if (isset($_SESSION["user"])) {
+    echo '<div class="container pt-5 ">
+
+    <h1 class="display-2 text-center" style="font-weight: bold; padding-top:7%">You are already logged in </h1>
+</div>
+';
+}else{
+    require("./view/login.phtml");
+    
+}
 require("./view/footer.phtml");
